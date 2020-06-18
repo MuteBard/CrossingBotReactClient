d5a123cb63e724551381fdfbe1248bb1098dac68
MuteBard <csevere@brandeis.edu>
2020-06-18 18:49:42 -0400

    clear warnings

--- src/Pages/Loading.js
+++ src/Pages/Loading.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import LoadingSVG from '../Assets/resolved/loadingsvg'
-import {Redirect} from 'react-router-dom';
 
 export default class Loading extends Component {
     render() {
